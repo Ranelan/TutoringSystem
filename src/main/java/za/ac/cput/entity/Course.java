ddfeee79f7fcf32eb520001ce4deec670c3ab6ae
Scelo Kevin Nyandeni <scelokevinnyandeni22@gmail.com>
2025-03-20 22:23:57 +0200

    Update Course.java
    Updated Course class

--- src/main/java/za/ac/cput/entity/Course.java
+++ src/main/java/za/ac/cput/entity/Course.java
@@ -1,12 +1,93 @@
 /*
 Enrollment.java
 Author: Ranelani Engel (221813853)
+Author : Scelo Kevin Nyandeni (230189695)
 Date: 20 March 2025
  */
 
 
 package za.ac.cput.entity;
+import za.ac.cput.Tutor;
 
 public class Course {
+    private int courseId;
+    private String courseName;
+    private String courseDescription;
+    private Tutor tutor;
 
+    private Course() {
+
+    }
+    
+    private Course(Builder builder) {
+        this.courseId = builder.courseId;
+        this.courseName = builder.courseName;
+        this.courseDescription = builder.courseDescription;
+        this.tutor = builder.tutor;
+        
+    }
+    public Course(int courseId, String courseName, String courseDescription, Tutor tutor) {
+        this.courseId = courseId;
+        this.courseName = courseName;
+        this.courseDescription = courseDescription;
+        this.tutor = tutor;
+    }
+
+    public int getCourseId() {
+        return courseId;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public String getCourseDescription() {
+        return courseDescription;
+    }
+
+    public Tutor getTutor() {
+        return tutor;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "courseId=" + courseId +
+                ", courseName='" + courseName + '\'' +
+                ", courseDescription='" + courseDescription + '\'' +
+                ", tutor=" + tutor +
+                '}';
+    }
+    
+    public static class Builder {
+        private int courseId;
+        private String courseName;
+        private String courseDescription;
+        private Tutor tutor;
+
+        public Builder setCourseId(int courseId) {
+            this.courseId = courseId;
+            return this;
+        }
+
+        public Builder setCourseName(String courseName) {
+            this.courseName = courseName;
+            return this;
+        }
+
+        public Builder setCourseDescription(String courseDescription) {
+            this.courseDescription = courseDescription;
+            return this;
+        }
+
+        public Builder setTutor(Tutor tutor) {
+            this.tutor = tutor;
+            return this;
+        }
+        public Course build(){
+            return new Course(this);
+            
+        }
+    }
+    
 }
