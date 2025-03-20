6ef57c80f844895f233a3a12f33ef878e9ee5900
smangasmashntuli <230070728@mycput.ac.za>
2025-03-20 17:14:08 +0200

    Assignment class

---
+++ src/main/java/za/ac/cput/domain/Assignment.java
@@ -0,0 +1,101 @@
+/*
+Assignment.java
+Author: Ntuli Simangaliso Mazweni (230070728)
+Date: 20 March 2025
+ */
+
+
+package za.ac.cput.domain;
+
+import java.time.LocalDate;
+
+public class Assignment {
+    private int assignmentId;
+    private String title;
+    private String description;
+    private LocalDate dueDate;
+    private int maxPoints;
+
+    public Assignment() {
+    }
+
+    public Assignment(AssignmentBuilder assignmentBuilder){
+        this.assignmentId = assignmentBuilder.assignmentId;
+        this.title = assignmentBuilder.title;
+        this.description = assignmentBuilder.description;
+        this.dueDate = assignmentBuilder.dueDate;
+        this.maxPoints = assignmentBuilder.maxPoints;
+    }
+
+    public int getAssignmentId() {
+        return assignmentId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public int getMaxPoints() {
+        return maxPoints;
+    }
+
+    @Override
+    public String toString() {
+        return "Assignment{" +
+                "assignmentId=" + assignmentId +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", dueDate=" + dueDate +
+                ", maxPoints=" + maxPoints +
+                '}';
+    }
+
+    public static class AssignmentBuilder {
+        private int assignmentId;
+        private String title;
+        private String description;
+        private LocalDate dueDate;
+        private int maxPoints;
+
+        public AssignmentBuilder() {
+
+        }
+
+        public AssignmentBuilder setAssignmentId(int assignmentId) {
+            this.assignmentId = assignmentId;
+            return this;
+        }
+
+        public AssignmentBuilder setTitle(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public AssignmentBuilder setDescription(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public AssignmentBuilder setDueDate(LocalDate dueDate) {
+            this.dueDate = dueDate;
+            return this;
+        }
+
+        public AssignmentBuilder setMaxPoints(int maxPoints) {
+            this.maxPoints = maxPoints;
+            return this;
+        }
+
+        public Assignment build() {
+            return new Assignment(this);
+        }
+    }
+}
