9b6896fee0ca0e31b0ce678f86f3ea808efd1f27
Engel Ranelani <139503058+Ranelan@users.noreply.github.com>
2025-03-28 01:09:11 +0200

    Delete src/main/java/za/ac/cput/Repository/IAssignmentRepository.java

--- src/main/java/za/ac/cput/Repository/IAssignmentRepository.java
+++
@@ -1,9 +0,0 @@
-package za.ac.cput.Repository;
-
-import za.ac.cput.Entity.Assignment;
-
-import java.util.List;
-
-public interface IAssignmentRepository extends IRepository<Assignment, String>{
-    List<Assignment> getAll();
-}
